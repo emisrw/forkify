46dd63f9e1c47132a5d727ba5ad833abbd8d1698
emisrw <emisrwlk@gmail.com>
2019-05-05 20:19:36 +0200

    Api calls by axios

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,19 @@
-// Global app controller
-import num from './test';
-const x =23;
-console.log(`i imported ${num} from test var x ${x}`);
+import axios from 'axios';
+const key = 'aab4009403987c6c967ae5005ba71d03ad';
+// 
+//https://www.food2fork.com/api/search
+
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    try {
+      const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+      const recipes = res.data.recipes;
+      console.log(recipes);
+    }catch(error) {
+      alert(error);
+    }
+ 
+   
+}
+
+getResults('pizza');
